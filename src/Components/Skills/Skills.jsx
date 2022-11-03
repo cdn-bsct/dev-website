37a7b1bedde9fefd865a09903ad36f368d40f704
cdn-bsct <justinarfeldman@gmail.com>
2022-11-03 11:12:58 -0400

    skill list add

--- src/Components/Skills/Skills.jsx
+++ src/Components/Skills/Skills.jsx
@@ -3,8 +3,20 @@ import "./Skills.css"
 export default function Skills(props) {
     return (
         <div className="skills-container">
-            <hr />
             <img src="https://i.imgur.com/pAz8ATd.png" alt="skills-img" />
+            <div className="skill-list">
+                <li className="alt-1">HTML</li>
+                <li className="alt-2">CSS</li>
+                <li className="alt-1">JavaScript</li>
+                <li className="alt-2">Express</li>
+                <li className="alt-1">Node.js</li>
+                <li className="alt-2">Python</li>
+                <li className="alt-1">MongoDB</li>
+                <li className="alt-2">React</li>
+                <li className="alt-1">Git & GitHub</li>
+                <li className="alt-2">PostgreSQL</li>
+                <li className="alt-1">Django</li>
+            </div>
         </div>
     )
 }
